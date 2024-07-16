a1500d51d1ffe2a65ebb2dcd84cbbf953616a901
rtanlydia <rtanlydia@gmail.com>
2024-07-16 05:22:23 -0700

    update

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -16,6 +16,12 @@ export default function WorkingWithArrays(app) {
         todos.push(newTodo);
         res.json(todos);
     });
+    app.post("/lab5/todos", (req, res) => {
+        const newTodo = { ...req.body,  id: new Date().getTime() };
+        todos.push(newTodo);
+        res.json(newTodo);
+    });
+
 
     app.get("/lab5/todos", (req, res) => {
         const { completed } = req.query;
@@ -40,6 +46,17 @@ export default function WorkingWithArrays(app) {
         todos.splice(todoIndex, 1);
         res.json(todos);
     });
+    app.delete("/lab5/todos/:id", (req, res) => {
+        const { id } = req.params;
+        const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+        if (todoIndex === -1) {
+            res.status(404).json({ message: `Unable to delete Todo with ID ${id}` });
+            return;
+        }
+        todos.splice(todoIndex, 1);
+        res.sendStatus(200);
+    });
+
 
     app.get("/lab5/todos/:id/title/:title", (req, res) => {
         const { id, title } = req.params;
@@ -61,4 +78,16 @@ export default function WorkingWithArrays(app) {
         todo.description = description;
         res.json(todos);
     });
+    app.put("/lab5/todos/:id", (req, res) => {
+        const { id } = req.params;
+
+        todos = todos.map((t) => {
+            if (t.id === parseInt(id)) {
+                return { ...t, ...req.body };
+            }
+            return t;
+        });
+        res.sendStatus(200);
+    });
+
 }
