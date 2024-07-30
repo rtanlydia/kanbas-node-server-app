1a4637038091650272f72dcfa4381b43c13cf641
rongtan <tan.ro@northeastern.edu>
2024-07-30 14:56:38 +0800

    update a6

--- Users/dao.js
+++ Users/dao.js
@@ -1,15 +1,29 @@
 import model from "./model.js";
-export const createUser = (user) => {} // implemented later
+export const createUser = (user) => {
+    delete user._id
+    return model.create(user);
+}
+
 export const findAllUsers = () => model.find();
 export const findUserById = (userId) => model.findById(userId);
 export const findUserByUsername = (username) =>  model.findOne({ username: username });
 export const findUserByCredentials = (username, password) =>  model.findOne({ username, password });
 export const updateUser = (userId, user) =>  model.updateOne({ _id: userId }, { $set: user });
+
 export const deleteUser = (userId) => model.deleteOne({ _id: userId });
+
 export const findUsersByRole = (role) => model.find({ role: role }); // or just model.find({ role })
 export const findUsersByPartialName = (partialName) => {
     const regex = new RegExp(partialName, "i"); // 'i' makes it case-insensitive
     return model.find({
         $or: [{ firstName: { $regex: regex } }, { lastName: { $regex: regex } }],
     });
+};
+
+export const updateUserEmail = (userId, email) => {
+    return model.updateOne({ _id: userId }, { $set: { email: email } });
+};
+
+export const updateUserRole = (userId, role) => {
+    return model.updateOne({ _id: userId }, { $set: { role: role } });
 };
--- Users/routes.js
+++ Users/routes.js
@@ -2,7 +2,10 @@ import * as dao from "./dao.js";
 let currentUser = null;
 export default function UserRoutes(app) {
 
-    const createUser = async (req, res) => { };
+    const createUser = async (req, res) => {
+        const user = await dao.createUser(req.body);
+        res.json(user);
+    };
     const deleteUser = async (req, res) => {
         const status = await dao.deleteUser(req.params.userId);
         res.json(status);
@@ -30,7 +33,11 @@ export default function UserRoutes(app) {
         const user = await dao.findUserById(req.params.userId);
         res.json(user);
     };
-    const updateUser = async (req, res) => { };
+    const updateUser = async (req, res) => {
+        const { userId } = req.params;
+        const status = await dao.updateUser(userId, req.body);
+        res.json(status);
+    };
     const signup = async (req, res) => { };
     const signin = async (req, res) => { };
     const signout = (req, res) => { };
@@ -41,12 +48,15 @@ export default function UserRoutes(app) {
     app.get("/api/users", findAllUsers);
     app.get("/api/users/:userId", findUserById);
     app.put("/api/users/:userId", updateUser);
+    app.put("/api/users/:userId/role", updateUserRole);
     app.delete("/api/users/:userId", deleteUser);
     app.post("/api/users/signup", signup);
     app.post("/api/users/signin", signin);
     app.post("/api/users/signout", signout);
     app.post("/api/users/profile", profile);
-}
+    app.put("/api/users/:userId/email", updateUserEmail);}
+
+
 
 const findAllUsers = async (req, res) => {
     const { role } = req.query;
@@ -58,5 +68,19 @@ const findAllUsers = async (req, res) => {
     const users = await dao.findAllUsers();
     res.json(users);
     return;
+};
+
+const updateUserEmail = async (req, res) => {
+    const { userId } = req.params;
+    const { email } = req.body;
+    const status = await dao.updateUserEmail(userId, email);
+    res.json(status);
+};
+
+const updateUserRole = async (req, res) => {
+    const { userId } = req.params;
+    const { role } = req.body;
+    const status = await dao.updateUserRole(userId, role);
+    res.json(status);
 };
 
