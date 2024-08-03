e0eacab95357ea9c7c410567c05c279343f221a9
rongtan <tan.ro@northeastern.edu>
2024-08-03 19:15:06 +0800

    quizpage

--- Kanbas/Quizzes/routes.js
+++ Kanbas/Quizzes/routes.js
@@ -1,74 +1,130 @@
-// // import db from "../Database/index.js";
-// // export default function QuizRoutes(app) {
-// //     app.get("/api/courses/:cid/quizzes", (req, res) => {
+// // // import db from "../Database/index.js";
+// // // export default function QuizRoutes(app) {
+// // //     app.get("/api/courses/:cid/quizzes", (req, res) => {
+// // //         const { cid } = req.params;
+// // //         const  quizzes= db.quizzes.filter((a) => a.course === cid);
+// // //         res.json(quizzes);
+// // //     });
+// // //
+// // //     app.post("/api/courses/:cid/quizzes", (req, res) => {
+// // //         const { cid } = req.params;
+// // //         const newAssignment = {
+// // //             ...req.body,
+// // //             title: req.body.title || 'Untitled Assignment',
+// // //             course: cid,
+// // //             _id: new Date().getTime().toString(),
+// // //         };
+// // //         db.assignments.push(newAssignment);
+// // //         res.send(newAssignment);
+// // //     });
+// // //
+// // //     app.put("/api/quizzes/:aid", (req, res) => {
+// // //         const { aid } = req.params;
+// // //         const assignmentIndex = db.assignments.findIndex((a) => a._id === aid);
+// // //         if (assignmentIndex !== -1) {
+// // //             db.assignments[assignmentIndex] = {
+// // //                 ...db.assignments[assignmentIndex],
+// // //                 ...req.body,
+// // //             };
+// // //             res.sendStatus(204);
+// // //         } else {
+// // //             res.sendStatus(404);
+// // //         }
+// // //     });
+// // //
+// // //     app.delete("/api/quizzes/:aid", (req, res) => {
+// // //         const { aid } = req.params;
+// // //         db.assignments = db.assignments.filter((a) => a._id !== aid);
+// // //         res.sendStatus(200);
+// // //     });
+// // // }
+// //
+// //
+// // import * as dao from "../Assignments/dao.js";
+// //
+// // const QuizRoutes = (app) => {
+// //     app.get("/api/courses/:cid/assignments", async (req, res) => {
 // //         const { cid } = req.params;
-// //         const  quizzes= db.quizzes.filter((a) => a.course === cid);
-// //         res.json(quizzes);
+// //         const assignments = await dao.findAssignmentsByCourse(cid);
+// //         res.json(assignments);
 // //     });
 // //
-// //     app.post("/api/courses/:cid/quizzes", (req, res) => {
+// //     app.post("/api/courses/:cid/assignments", async (req, res) => {
 // //         const { cid } = req.params;
 // //         const newAssignment = {
 // //             ...req.body,
-// //             title: req.body.title || 'Untitled Assignment',
+// //             title: req.body.title || "Untitled Assignment",
 // //             course: cid,
-// //             _id: new Date().getTime().toString(),
 // //         };
-// //         db.assignments.push(newAssignment);
-// //         res.send(newAssignment);
+// //         const createdAssignment = await dao.createAssignment(newAssignment);
+// //         res.json(createdAssignment);
 // //     });
 // //
-// //     app.put("/api/quizzes/:aid", (req, res) => {
+// //     app.put("/api/assignments/:aid", async (req, res) => {
 // //         const { aid } = req.params;
-// //         const assignmentIndex = db.assignments.findIndex((a) => a._id === aid);
-// //         if (assignmentIndex !== -1) {
-// //             db.assignments[assignmentIndex] = {
-// //                 ...db.assignments[assignmentIndex],
-// //                 ...req.body,
-// //             };
-// //             res.sendStatus(204);
+// //         const updatedAssignment = await dao.updateAssignment(aid, req.body);
+// //         res.json(updatedAssignment)
+// //     });
+// //
+// //     app.delete("/api/assignments/:aid", async (req, res) => {
+// //         const { aid } = req.params;
+// //         const deleteStatus = await dao.deleteAssignment(aid);
+// //         if (deleteStatus.deletedCount > 0) {
+// //             res.sendStatus(200);
 // //         } else {
 // //             res.sendStatus(404);
 // //         }
 // //     });
 // //
-// //     app.delete("/api/quizzes/:aid", (req, res) => {
+// //     app.get("/api/assignments/:aid", async (req, res) => {
 // //         const { aid } = req.params;
-// //         db.assignments = db.assignments.filter((a) => a._id !== aid);
-// //         res.sendStatus(200);
+// //         try {
+// //             const assignment = await dao.findAssignmentById(aid);
+// //             if (assignment) {
+// //                 res.json(assignment);
+// //             } else {
+// //                 res.status(404).json({ error: 'Assignment not found' });
+// //             }
+// //         } catch (error) {
+// //             res.status(500).json({ error: 'Internal server error' });
+// //         }
 // //     });
-// // }
+// //
+// //
+// // };
+// //
+// // export default QuizRoutes;
 //
 //
-// import * as dao from "../Assignments/dao.js";
+// import * as dao from "./dao.js";
 //
 // const QuizRoutes = (app) => {
-//     app.get("/api/courses/:cid/assignments", async (req, res) => {
+//     app.get("/api/courses/:cid/quizzes", async (req, res) => {
 //         const { cid } = req.params;
-//         const assignments = await dao.findAssignmentsByCourse(cid);
-//         res.json(assignments);
+//         const quizzes = await dao.findQuizzesByCourse(cid);
+//         res.json(quizzes);
 //     });
 //
-//     app.post("/api/courses/:cid/assignments", async (req, res) => {
+//     app.post("/api/courses/:cid/quizzes", async (req, res) => {
 //         const { cid } = req.params;
-//         const newAssignment = {
+//         const newQuiz = {
 //             ...req.body,
-//             title: req.body.title || "Untitled Assignment",
+//             title: req.body.title || "Untitled Quiz",
 //             course: cid,
 //         };
-//         const createdAssignment = await dao.createAssignment(newAssignment);
-//         res.json(createdAssignment);
+//         const createdQuiz = await dao.createQuiz(newQuiz);
+//         res.json(createdQuiz);
 //     });
 //
-//     app.put("/api/assignments/:aid", async (req, res) => {
-//         const { aid } = req.params;
-//         const updatedAssignment = await dao.updateAssignment(aid, req.body);
-//         res.json(updatedAssignment)
+//     app.put("/api/quizzes/:qid", async (req, res) => {
+//         const { qid } = req.params;
+//         const updatedQuiz = await dao.updateQuiz(qid, req.body);
+//         res.json(updatedQuiz);
 //     });
 //
-//     app.delete("/api/assignments/:aid", async (req, res) => {
-//         const { aid } = req.params;
-//         const deleteStatus = await dao.deleteAssignment(aid);
+//     app.delete("/api/quizzes/:qid", async (req, res) => {
+//         const { qid } = req.params;
+//         const deleteStatus = await dao.deleteQuiz(qid);
 //         if (deleteStatus.deletedCount > 0) {
 //             res.sendStatus(200);
 //         } else {
@@ -76,21 +132,19 @@
 //         }
 //     });
 //
-//     app.get("/api/assignments/:aid", async (req, res) => {
-//         const { aid } = req.params;
+//     app.get("/api/quizzes/:qid", async (req, res) => {
+//         const { qid } = req.params;
 //         try {
-//             const assignment = await dao.findAssignmentById(aid);
-//             if (assignment) {
-//                 res.json(assignment);
+//             const quiz = await dao.findQuizById(qid);
+//             if (quiz) {
+//                 res.json(quiz);
 //             } else {
-//                 res.status(404).json({ error: 'Assignment not found' });
+//                 res.status(404).json({ error: 'Quiz not found' });
 //             }
 //         } catch (error) {
 //             res.status(500).json({ error: 'Internal server error' });
 //         }
 //     });
-//
-//
 // };
 //
 // export default QuizRoutes;
--- Kanbas/Quizzes/schema.js
+++ Kanbas/Quizzes/schema.js
@@ -1,13 +1,75 @@
+// import mongoose from "mongoose";
+//
+// const quizSchema = new mongoose.Schema({
+//     title: { type: String, default: "Untitled Quiz" },
+//     course: { type: String},
+//     description: { type: String, default: "" },
+//     availableFrom: { type: Date, default: null },
+//     availableUntil: { type: Date, default: null },
+//     dueDate: { type: Date, default: null },
+//     points: { type: Number, default: 100 },
+//     numberOfQuestion : {type: Number, default: 40}
+// }, { collection: "quizzes" });
+//
+// export default quizSchema;
+
+// import mongoose from "mongoose";
+//
+// const quizSchema = new mongoose.Schema({
+//     title: { type: String, default: "Untitled Quiz" },
+//     course: { type: String },
+//     description: { type: String, default: "" },
+//     availableFrom: { type: Date, default: null },
+//     availableUntil: { type: Date, default: null },
+//     dueDate: { type: Date, default: null },
+//     points: { type: Number, default: 100 },
+//     numberOfQuestions: { type: Number, default: 40 },
+//     status: {
+//         type: String,
+//         enum: ["Closed", "Available", "Not available until <AVAILABLE DATE>"],
+//         default: function() {
+//             const currentDate = new Date();
+//             if (this.availableFrom && currentDate < this.availableFrom) {
+//                 return `Not available until ${this.availableFrom.toLocaleDateString()}`;
+//             } else if (this.availableUntil && currentDate > this.availableUntil) {
+//                 return "Closed";
+//             } else {
+//                 return "Available";
+//             }
+//         }
+//     },
+//     score: { type: Number, default: null }
+// }, { collection: "quizzes" });
+//
+// export default quizSchema;
+
+
 import mongoose from "mongoose";
 
 const quizSchema = new mongoose.Schema({
     title: { type: String, default: "Untitled Quiz" },
-    course: { type: String},
+    course: { type: String },
     description: { type: String, default: "" },
     availableFrom: { type: Date, default: null },
     availableUntil: { type: Date, default: null },
     dueDate: { type: Date, default: null },
-    points: { type: Number, default: 100 }
+    points: { type: Number, default: 100 },
+    numberOfQuestions: { type: Number, default: 40 },
+    score: { type: Number, default: null }
 }, { collection: "quizzes" });
+
+quizSchema.virtual('status').get(function() {
+    const currentDate = new Date();
+    if (this.availableFrom && currentDate < this.availableFrom) {
+        return `Not available until ${this.availableFrom.toLocaleDateString()}`;
+    } else if (this.availableUntil && currentDate > this.availableUntil) {
+        return "Closed";
+    } else {
+        return "Available";
+    }
+});
+
+quizSchema.set('toJSON', { virtuals: true });
+quizSchema.set('toObject', { virtuals: true });
 
 export default quizSchema;
